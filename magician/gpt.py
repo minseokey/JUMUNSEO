f4bc54f88eed7ffc3e86f8359c1982ec7219a9e1
minseokey <lms990427@kookmin.ac.kr>
2024-03-17 00:59:02 +0900

    Feat: add gpt_reload func

--- magician/gpt.py
+++ magician/gpt.py
@@ -23,3 +23,15 @@ async def gpt_talk(msg, additional_prompt=""):
     except Exception as e:
         print(e)
         return None
+
+
+async def gpt_reload(msg):
+    try:
+        client.chat.completions.create(model="gpt-3.5-turbo",
+                                       messages=[
+                                           {"role": "system",
+                                            "content": "너는 한국의 법령과 판례를 이용해서 수임자를 도와주는 법조인이야,"
+                                                       " 법을 잘 모르는 한국인을 대상으로 도움을 주는 이전에 " + msg +
+                                                       "에 대한 대화를 했었어. 이제 이어서 대화할거야"}])
+    except Exception as e:
+        print(e)
