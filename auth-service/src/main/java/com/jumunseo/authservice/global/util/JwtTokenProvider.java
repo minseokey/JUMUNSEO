2c474fecb3b82e1b5c5229ac5157ed814501b231
minseokey <lms990427@kookmin.ac.kr>
2024-03-19 08:45:23 +0900

    Feat: add JWT Token provider

---
+++ auth-service/src/main/java/com/jumunseo/authservice/global/util/JwtTokenProvider.java
@@ -0,0 +1,43 @@
+package com.jumunseo.authservice.global.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access_expiration}")
+    private long accessTokenExpiration;
+
+    @Value("${jwt.refresh_expiration}")
+    private long refreshTokenExpiration;
+
+    public String createAccessToken(String userId, String role) {
+        Claims claims = Jwts.claims().setSubject(userId);
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String createRefreshToken() {
+        Claims claims = Jwts.claims();
+        claims.put("UUID", UUID.randomUUID().toString());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
