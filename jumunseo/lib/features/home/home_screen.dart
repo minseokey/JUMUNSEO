1cfe32ad704a9f349504363260a2d464efddb28d
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-04-04 13:51:53 +0900

    .

--- jumunseo/lib/features/home/home_screen.dart
+++ jumunseo/lib/features/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jumunseo/core/logger.dart';
+import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> homeWidgets = ["마법사", "게시판", "딜레마"];
+
   @override
   Widget build(BuildContext context) {
     logger.d('HomeScreen build');
@@ -24,112 +27,143 @@ class _HomeScreenState extends State<HomeScreen> {
           Expanded(
             child: SingleChildScrollView(
               physics: const AlwaysScrollableScrollPhysics(),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: SizedBox(
-                                  height: 100,
-                                  child: Material(
-                                      //color: Colors.red.shade200,
-                                      color: Colors.transparent,
-                                      child: InkWell(
-                                        onTap: () {
-                                          context.push('/wizard');
-                                        },
-                                        child: const Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Icon(Icons.add, size: 50),
-                                            Text("마법사"),
-                                          ],
-                                        ),
-                                      )))),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: SizedBox(
-                                  height: 100,
-                                  child: Material(
-                                      //color: Colors.orange.shade200,
-                                      color: Colors.transparent,
-                                      child: InkWell(
-                                        onTap: () {
-                                          context.push('/dilemma/1/1');
-                                        },
-                                        child: const Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Icon(Icons.add, size: 50),
-                                            Text("딜레마 해결 서비스"),
-                                          ],
-                                        ),
-                                      )))),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        const Text("게시판"),
-                        const Spacer(),
-                        Material(
-                            child: InkWell(
-                                onTap: () {
-                                  context.push('/community');
-                                },
-                                child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15.0),
-                                      color: Colors.blue.shade200,
-                                    ),
-                                    child: const Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Text("더보기"),
-                                    )))),
-                      ],
-                    ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                height: 100,
-                                color: const Color.fromARGB(190, 35, 20, 235),
-                              ),
-                            ),
-                          ],
+              child: ReorderableGridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 1.5,
+                onReorder: ((oldIndex, newIndex) {
+                  String tmp = homeWidgets.removeAt(oldIndex);
+                  homeWidgets.insert(newIndex, tmp);
+                  setState(() {});
+                }),
+                children: homeWidgets.map((widget) => Card(
+                  key: ValueKey(widget),
+                  child: GestureDetector(
+                      onTapUp: (details) {
+                        if(widget == '마법사') {
+                          context.push("/wizard");
+                        }
+                        else if(widget == "게시판") {
+                          context.push("/community");
+                        }
+                        else if(widget == "딜레마") {
+                          context.push("/dilemma/1/1");
+                        }
+                      },
+                      child: Expanded(
+                        child: Container(
+                          decoration: const BoxDecoration(color: Colors.purple),
+                          child: Text(widget),
                         ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                )).toList(),
+                ),
+              // child: Column(
+              //   children: <Widget>[
+              //     Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Row(
+              //         children: <Widget>[
+              //           Expanded(
+              //             child: Card(
+              //                 shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(15.0),
+              //                 ),
+              //                 child: SizedBox(
+              //                     height: 100,
+              //                     child: Material(
+              //                         //color: Colors.red.shade200,
+              //                         color: Colors.transparent,
+              //                         child: InkWell(
+              //                           onTap: () {
+              //                             context.push('/wizard');
+              //                           },
+              //                           child: const Column(
+              //                             mainAxisAlignment:
+              //                                 MainAxisAlignment.center,
+              //                             children: <Widget>[
+              //                               Icon(Icons.add, size: 50),
+              //                               Text("마법사"),
+              //                             ],
+              //                           ),
+              //                         )))),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //     Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Row(
+              //         children: <Widget>[
+              //           Expanded(
+              //             child: Card(
+              //                 shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(15.0),
+              //                 ),
+              //                 child: SizedBox(
+              //                     height: 100,
+              //                     child: Material(
+              //                         //color: Colors.orange.shade200,
+              //                         color: Colors.transparent,
+              //                         child: InkWell(
+              //                           onTap: () {
+              //                             context.push('/dilemma/1/1');
+              //                           },
+              //                           child: const Column(
+              //                             mainAxisAlignment:
+              //                                 MainAxisAlignment.center,
+              //                             children: <Widget>[
+              //                               Icon(Icons.add, size: 50),
+              //                               Text("딜레마 해결 서비스"),
+              //                             ],
+              //                           ),
+              //                         )))),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //     Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Row(
+              //         children: <Widget>[
+              //           const Text("게시판"),
+              //           const Spacer(),
+              //           Material(
+              //               child: InkWell(
+              //                   onTap: () {
+              //                     context.push('/community');
+              //                   },
+              //                   child: Container(
+              //                       decoration: BoxDecoration(
+              //                         borderRadius: BorderRadius.circular(15.0),
+              //                         color: Colors.blue.shade200,
+              //                       ),
+              //                       child: const Padding(
+              //                         padding: EdgeInsets.all(8.0),
+              //                         child: Text("더보기"),
+              //                       )))),
+              //         ],
+              //       ),
+              //     ),
+              //     Column(
+              //       children: <Widget>[
+              //         Padding(
+              //           padding: const EdgeInsets.all(8.0),
+              //           child: Row(
+              //             children: <Widget>[
+              //               Expanded(
+              //                 child: Container(
+              //                   height: 100,
+              //                   color: const Color.fromARGB(190, 35, 20, 235),
+              //                 ),
+              //               ),
+              //             ],
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ],
+              // ),
             ),
           ),
         ],
