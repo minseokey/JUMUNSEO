2a598b32be5d6f3b40131ae8fe13ae893513c0f4
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-04-08 16:52:05 +0900

    Fix: fix reorderablelistview crop border problem

--- jumunseo/lib/features/home/view/home_screen.dart
+++ jumunseo/lib/features/home/view/home_screen.dart
@@ -18,6 +18,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late List<String> homeWidgets;
 
+  Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget? child) {
+        return Material(
+          elevation: 0,
+          color: Colors.transparent,
+          child: child,
+        );
+      },
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     logger.d('HomeScreen build');
@@ -31,6 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         child: ReorderableListView(
+          proxyDecorator: proxyDecorator,
           onReorder: ((oldIndex, newIndex) {
             String tmp = homeWidgets.removeAt(oldIndex);
             homeWidgets.insert(newIndex, tmp);
@@ -39,9 +54,9 @@ class _HomeScreenState extends State<HomeScreen> {
           }),
           children: homeWidgets.map((e) => 
             Padding(
-              key: Key(e),
               padding: const EdgeInsets.symmetric(vertical: 15.0),
-              child: (e == '마법사')? 
+              key: Key(e),
+              child:(e == '마법사')? 
                 GestureDetector(
                   onTapUp: (details) => context.read<HomeCubit>().homeToWizard(context),
                   child: const WizardMenu()
@@ -51,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: const DilemaMenu()
                 ): 
                 const CommunityMenu(),
-            )).toList(),
+              )).toList(),
           ),
       ),
     );
