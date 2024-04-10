31039dae51da15fc68bd617959119753693d5dfc
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-04-10 20:59:07 +0900

    Feat: home add appbar title logo and profile, and add refresh

--- jumunseo/lib/features/home/view/home_screen.dart
+++ jumunseo/lib/features/home/view/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import '../home.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jumunseo/core/blank.dart';
 import 'package:jumunseo/core/logger.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -37,53 +36,71 @@ class _HomeScreenState extends State<HomeScreen> {
     homeWidgets = context.read<HomeCubit>().getHomeList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-            const Text('홈'),
-            const Spacer(),
-            GestureDetector(
-              onTapUp: (details) {
-                context.read<HomeCubit>().hometoProfile(context);
-              },
-              child: ExtendedImage.asset('assets/icons/setting.png', width: 40.0, height: 40.0,),
+          children: [
+            AppBar(
+              toolbarHeight: 50,
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Row(
+                  children:[
+                    ExtendedImage.asset('assets/icons/logo.png', width: 50, height: 50,),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: ExtendedImage.asset('assets/icons/profile.png', width: 50, height: 50,),
+                    ),
+                    GestureDetector(
+                      onTapUp: (details) {
+                        context.read<HomeCubit>().hometoProfile(context);
+                      },
+                      child: const Icon(Icons.settings),
+                    ),
+                  ]
+                ),
+              ),
             ),
-            const Blank(10, 0),
           ]
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: ReorderableListView(
-          proxyDecorator: proxyDecorator,
-          onReorder: ((oldIndex, newIndex) {
-            setState(() {
-              if (oldIndex < newIndex) {
-                newIndex -= 1;
-              }
-              final String item = homeWidgets.removeAt(oldIndex);
-              homeWidgets.insert(newIndex, item);
-              context.read<HomeCubit>().putHomeList(homeWidgets);
-            });
-          }),
-          children: homeWidgets.map((e) => 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15.0),
-              key: Key(e),
-              child:(e == '마법사')? 
-                GestureDetector(
-                  onTapUp: (details) => context.read<HomeCubit>().homeToWizard(context),
-                  child: const WizardMenu()
-                ): (e == '딜레마')? 
-                GestureDetector(
-                  onTapUp: (details) => context.read<HomeCubit>().homeToDilema(context),
-                  child: const DilemaMenu()
-                ): 
-                const CommunityMenu(),
-              )).toList(),
-          ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: ReorderableListView(
+            proxyDecorator: proxyDecorator,
+            onReorder: ((oldIndex, newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final String item = homeWidgets.removeAt(oldIndex);
+                homeWidgets.insert(newIndex, item);
+                context.read<HomeCubit>().putHomeList(homeWidgets);
+              });
+            }),
+            children: homeWidgets.map((e) => 
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                key: Key(e),
+                child:(e == '마법사')? 
+                  GestureDetector(
+                    onTapUp: (details) => context.read<HomeCubit>().homeToWizard(context),
+                    child: const WizardMenu()
+                  ): (e == '딜레마')? 
+                  GestureDetector(
+                    onTapUp: (details) => context.read<HomeCubit>().homeToDilema(context),
+                    child: const DilemaMenu()
+                  ): 
+                  const CommunityMenu(),
+                )).toList(),
+            ),
+        ),
       ),
     );
   }
