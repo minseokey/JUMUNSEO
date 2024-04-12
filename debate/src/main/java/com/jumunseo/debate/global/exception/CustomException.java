1c8e6e2fde29d9907871eced361f98bd4ae3af65
minseokey <lms990427@kookmin.ac.kr>
2024-04-12 12:44:49 +0900

    Feat: add CustomException which extends RuntimeException

---
+++ debate/src/main/java/com/jumunseo/debate/global/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.jumunseo.debate.global.exception;
+
+import com.jumunseo.debate.global.dto.Result;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final HttpStatus httpStatus;
+    private final Result<?> result;
+
+    public CustomException(HttpStatus httpStatus, Result<?> result) {
+        this.httpStatus = httpStatus;
+        this.result = Result.failResult(result.getMessage());
+    }
+}
