57acef85889b507ef824bd27579ec5ec93dbdce3
K <kms1025@kookmin.ac.kr>
2024-04-12 12:05:26 +0900

    feat : 상태관리

--- jumunseo/lib/shared/blocs.dart
+++ jumunseo/lib/shared/blocs.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jumunseo/features/community/cubit/community_cubit.dart';
+import 'package:jumunseo/features/dilemma/category/bloc/dilemma_category_bloc.dart';
+import 'package:jumunseo/features/dilemma/chat/bloc/dilemma_chat_bloc.dart';
+import 'package:jumunseo/features/home/cubit/home_cubit.dart';
 import 'package:jumunseo/features/wizard/chat/chat.dart';
 
 class BlocWidget extends StatelessWidget {
@@ -11,8 +15,19 @@ class BlocWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<HomeCubit>(
+          create: (BuildContext context) => HomeCubit(),
+        ),
         BlocProvider<WizardCubit>(
           create: (BuildContext context) => WizardCubit(),
+        ),
+        BlocProvider<DilemmaCategoryBloc>(
+          create: (context) {
+            return DilemmaCategoryBloc();
+          },
+        ),
+        BlocProvider<CommunityCubit>(
+          create: (BuildContext context) => CommunityCubit(),
         ),
       ],
       child: child,
