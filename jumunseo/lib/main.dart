16e0258347a472829d968579e664baee048d794c
ggstargame45 <ggstargame45@kookmin.ac.kr>
2024-04-15 00:53:41 +0900

    feat : 오전/오후 표기를 위한 기본 intl세팅

--- jumunseo/lib/main.dart
+++ jumunseo/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:jumunseo/config/routes/app_routes.dart';
 import 'package:jumunseo/config/theme/app_theme.dart';
 import 'package:jumunseo/shared/blocs.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
   //TODO: 스플래시 스크린
@@ -20,12 +21,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocWidget(
       child: MaterialApp.router(
-        title: '주문서',
-        theme: theme(),
-        debugShowCheckedModeBanner: false,
-        routerConfig: appRouter,
-        locale: const Locale('ko'),
-      ),
+          title: '주문서',
+          theme: theme(),
+          debugShowCheckedModeBanner: false,
+          routerConfig: appRouter,
+          locale: const Locale('ko'),
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('ko'),
+          ]),
     );
   }
 }
