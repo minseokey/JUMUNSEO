a84e83e2ee004c31970d31a3595b5a81f0273ad2
ggstargame45 <ggstargame45@kookmin.ac.kr>
2024-04-16 22:51:33 +0900

    feature : front-web

--- jumunseo/lib/config/routes/app_routes.dart
+++ jumunseo/lib/config/routes/app_routes.dart
@@ -19,10 +19,30 @@ import 'package:jumunseo/features/wizard/chat/view/wizard_setting_view.dart';
 final GlobalKey<NavigatorState> rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
 
-const List<Widget> wizardDownRoutes = [CategoryView(), ChatView(), SettingView(), WhenView(),
-                                      WhoView(), WhereView(), HowView(), PlusInfoView(), WhatView(), WhyView()];
-const List<String> wizardDownRoutesPath = ['category', 'chat', 'chat_set', 'when', 'who', 'where', 'how', 'plus_info',
-                                            'what', 'why'];
+const List<Widget> wizardDownRoutes = [
+  CategoryView(),
+  ChatView(),
+  SettingView(),
+  WhenView(),
+  WhoView(),
+  WhereView(),
+  HowView(),
+  PlusInfoView(),
+  WhatView(),
+  WhyView()
+];
+const List<String> wizardDownRoutesPath = [
+  'category',
+  'chat',
+  'chat_set',
+  'when',
+  'who',
+  'where',
+  'how',
+  'plus_info',
+  'what',
+  'why'
+];
 
 GoRouter appRouter = GoRouter(
   navigatorKey: rootNavigatorKey,
@@ -49,25 +69,22 @@ GoRouter appRouter = GoRouter(
       ],
     ),
     GoRoute(
-      path: '/wizard',
-      builder: (context, state) {
-        return const RoomListView();
-      },
-      routes: List.generate(wizardDownRoutes.length, ((index) {
-        return GoRoute(
-          path: wizardDownRoutesPath[index],
-          builder: (context, state) {
-            return wizardDownRoutes[index];
-          }
-        );
-      }))
-    ),
+        path: '/wizard',
+        builder: (context, state) {
+          return const RoomListView();
+        },
+        routes: List.generate(wizardDownRoutes.length, ((index) {
+          return GoRoute(
+              path: wizardDownRoutesPath[index],
+              builder: (context, state) {
+                return wizardDownRoutes[index];
+              });
+        }))),
     GoRoute(
         path: '/community',
         builder: (context, state) {
           return const CommunityScreen();
         }),
-
     GoRoute(
         path: '/profile',
         builder: (context, state) {
