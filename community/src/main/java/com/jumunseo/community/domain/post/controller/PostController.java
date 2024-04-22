3185719b97122559045050c97f65853e796cb947
binary-h0 <max000807@gmail.com>
2024-04-22 15:25:59 +0900

    Feat: 커뮤니티 서비스 중 게시글 서비스인 PostController 생성 및 초기화

---
+++ community/src/main/java/com/jumunseo/community/domain/post/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.jumunseo.community.domain.post.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/community/posts")
+public class PostController {
+
+}
