e12073a94371ea1cc733dea7a23e5e13395b117b
binary-h0 <max000807@gmail.com>
2024-05-01 18:35:14 +0900

    Feat: DTO 수정

--- community/src/main/java/com/jumunseo/community/domain/post/dto/PostDto.java
+++ community/src/main/java/com/jumunseo/community/domain/post/dto/PostDto.java
@@ -1,8 +1,10 @@
 package com.jumunseo.community.domain.post.dto;
 
 import java.sql.Date;
+import java.util.List;
 
-import javax.xml.crypto.Data;
+import com.jumunseo.community.domain.post.entity.CategoryType;
+import com.jumunseo.community.domain.post.entity.Image;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -13,20 +15,30 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class PostDto {
+
     private Long id;
+    private CategoryType categoryId;
     private String userId;
     private Date createdAt;
     private Date updatedAt;
+    private Date deletedAt;
+    private Long viewCount;
     private String title;
     private String content;
+    private List<Image> images;
 
     @Builder
-    public PostDto(Long id, String userId, Date createdAt, Date updatedAt, String title, String content) {
+    public PostDto(Long id, CategoryType categoryId, String userId, Date createdAt, Date updatedAt, Date deletedAt,
+            Long viewCount, String title, String content, List<Image> images) {
         this.id = id;
+        this.categoryId = categoryId;
         this.userId = userId;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.viewCount = viewCount;
         this.title = title;
         this.content = content;
+        this.images = images;
     }
 }
