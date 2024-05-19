1e2882b792310fa828f08931f533fbf2175aa1a7
minseokey <lms990427@kookmin.ac.kr>
2024-05-20 05:17:00 +0900

    Feat: configure controllers for swagger

--- composite-service/src/main/java/com/jumunseo/compositeservice/command/controller/CommunityCommandController.java
+++ composite-service/src/main/java/com/jumunseo/compositeservice/command/controller/CommunityCommandController.java
@@ -1,4 +1,70 @@
 package com.jumunseo.compositeservice.command.controller;
 
+import com.jumunseo.compositeservice.command.service.RedisService;
+import com.jumunseo.compositeservice.global.exception.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/command/community")
+@Tag(name = "Community Command", description = "커뮤니티 서비스를 위한 커맨드 컨트롤러")
 public class CommunityCommandController {
+    private final RedisService redisService;
+
+    // 글 생성
+    @PostMapping("/board/create")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Posting Create", description = "게시글 생성 메소드")
+    public ResponseEntity<Result<?>> createBoard(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
+
+    // 글 삭제
+    @PostMapping("/board/delete")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Posting Delete", description = "게시글 삭제 메소드")
+    public ResponseEntity<Result<?>> deleteBoard(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
+
+
+    // 글 수정
+    @PostMapping("/board/update")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Posting Update", description = "게시글 수정 메소드")
+    public ResponseEntity<Result<?>> updateBoard(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
+
+    // 글 좋아요(토글)
+    @PostMapping("/board/like")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Posting Like", description = "게시글 좋아요 토글 메소드")
+    public ResponseEntity<Result<?>> toggleLike(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
+
+
+    // 댓글 생성
+    @PostMapping("/comment/create")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Comment Create", description = "댓글 생성 메소드")
+    public ResponseEntity<Result<?>> createComment(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
+
+    // 댓글 삭제
+    @PostMapping("/comment/delete")
+    @Tag(name = "Community Command")
+    @Operation(summary = "Comment Delete", description = "댓글 삭제 메소드")
+    public ResponseEntity<Result<?>> deleteComment(HttpServletRequest request) {
+        return ResponseEntity.ok(Result.successResult(null));
+    }
 }
--- composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
+++ composite-service/src/main/java/com/jumunseo/compositeservice/query/controller/CommunityQueryController.java
@@ -1,13 +1,58 @@
 package com.jumunseo.compositeservice.query.controller;
 
+import com.jumunseo.compositeservice.global.exception.Result;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/query/community")
 @Tag(name = "Communinty Query", description = "커뮤니티 서비스를 위한 쿼리 컨트롤러")
 public class CommunityQueryController {
+    @Value("${address.community}")
+    private String COMMUNITY_URL;
+    private final WebClient webClient;
+
+    // 커뮤니티 목록 조회
+    @GetMapping("/list")
+    @Tag(name = "Community Query")
+    @Operation(summary = "Community List", description = "커뮤니티 목록 조회")
+    private ResponseEntity<Result<?>> getCommunityList() {
+        return ResponseEntity.ok(null);
+    }
+
+    // 커뮤니티 목록 조회 with 카테고리
+    @GetMapping("/list/{category}")
+    @Tag(name = "Community Query")
+    @Operation(summary = "Community List with Category", description = "카테고리별 커뮤니티 목록 조회")
+    private ResponseEntity<Result<?>> getCommunityListWithCategory() {
+        return ResponseEntity.ok(null);
+    }
+
+
+    // 커뮤니티 상세 조회 -> 마법사 or 토론 요약 or 바닐라
+    @GetMapping("/detail/{id}")
+    @Tag(name = "Community Query")
+    @Operation(summary = "Community Detail", description = "커뮤니티 상세 조회")
+    private ResponseEntity<Result<?>> getCommunityDetail() {
+        return ResponseEntity.ok(null);
+    }
+
+
+    // 댓글 리스트 조회
+    @GetMapping("/comment/list/{id}")
+    @Tag(name = "Community Query")
+    @Operation(summary = "Comment List", description = "댓글 리스트 조회")
+    private ResponseEntity<Result<?>> getCommentList() {
+        return ResponseEntity.ok(null);
+    }
+
+
 }
